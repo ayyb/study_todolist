7ce538829f0f3ad65bc012274ec73e4c509c4598
ayyb <narabyeol@gmail.com>
2020-05-22 17:45:59 +0900

    initial file

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* 해당 컴포넌트만 정의되는 속성 scoped */
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
